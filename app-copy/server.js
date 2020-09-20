c22dd267fcad83993384b15621cd578873725a7e
Dima Dudin <nedudi@gmail.com>
2020-09-20 08:29:51 +0300

    fix

---
+++ app-copy/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8090;
+// const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+///console.log(`Running on http://${HOST}:${PORT}`);
